16c2218eafb8006e4af1a24e16b0d2d670599dfa
Ghadier-Alenezi <Ghadier.Mutar.elq.js@tuwaiq.edu.sa>
2021-11-17 06:16:33 +0300

    delete one task

--- back-end/index.js
+++ back-end/index.js
@@ -26,6 +26,25 @@ app.get("/todos", (req, res) => {
   });
 });
 
+// creat a new task
+app.delete("/todos/:id", (req, res) => {
+  fs.readFile("./db/tasks.json", (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      const { id } = req.params.id;
+      const todos = JSON.parse(data.toString());
+      for (let i = 0; i < todos.length; i++) {
+        const element = todos[i];
+        if (todos.id == id) {
+          todos.splice(element, 1);
+        }
+      }
+    }
+    res.status(200);
+    res.json(`the task is deleted`);
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`Server on ${PORT}`);
