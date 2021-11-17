1e7739cddc4105dd182cd9950e4d800eaa0a78ed
Ghadier-Alenezi <Ghadier.Mutar.elq.js@tuwaiq.edu.sa>
2021-11-17 05:23:34 +0300

    read all tasks in json file

--- back-end/index.js
+++ back-end/index.js
@@ -2,16 +2,31 @@ const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
 const dotenv = require("dotenv");
-
 const app = express();
+const fs = require("fs");
+
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(cors());
-require('dotenv').config()
+dotenv.config();
 
-const PORT= process.env.PORT || 4000;
+const PORT = process.env.PORT || 4000;
+
+// crud
+// read all tasks
+app.get("/todos", (req, res) => {
+  fs.readFile("./db/tasks.json", (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      const todos = JSON.parse(data.toString());
+      console.log(todos);
+      res.status(200).json(todos);
+    }
+  });
+});
 
 
-app.listen(PORT, ()=>{
-    console.log(`Server on ${PORT}`)
+app.listen(PORT, () => {
+  console.log(`Server on ${PORT}`);
 });
